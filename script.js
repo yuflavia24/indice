9ec83beed8c53147d770f2f678fd1c40d021d323
yuflavia24 <fla.studio.24@gmail.com>
2025-05-18 18:20:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let filas = [];
+let encabezados = [];
 
-async function cargarCSV() {
-  const respuesta = await fetch('datos.csv');
-  const texto = await respuesta.text();
+async function cargarDatos() {
+  const respuesta = await fetch('https://sheet.best/api/sheets/abc12345-fghi-6789-jklm-nopqrstuvwx');
+  const datos = await respuesta.json();
 
-  const lineas = texto.trim().split('\n');
-  const encabezados = lineas[0].split(',');
-
-  filas = lineas.slice(1).map(f => f.split(','));
+  encabezados = Object.keys(datos[0]);
+  filas = datos.map(obj => encabezados.map(key => obj[key]));
 
   const thead = document.querySelector('thead');
   thead.innerHTML = '<tr>' + encabezados.map(h => `<th>${h}</th>`).join('') + '</tr>';
@@ -19,7 +18,7 @@ function mostrarFilas(filasFiltradas) {
   const tbody = document.querySelector('tbody');
   tbody.innerHTML = '';
   filasFiltradas.forEach(fila => {
-    const filaHTML = '<tr>' + fila.map(celda => `<td>${celda}</td>`).join('') + '</tr>';
+    const filaHTML = '<tr>' + fila.map(celda => `<td>${celda || ''}</td>`).join('') + '</tr>';
     tbody.innerHTML += filaHTML;
   });
 }
@@ -32,4 +31,7 @@ function filtrarTabla() {
   mostrarFilas(filtradas);
 }
 
-cargarCSV();
+document.getElementById('entrada').addEventListener('input', filtrarTabla);
+
+cargarDatos();
+

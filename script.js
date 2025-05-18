107db6f109de9fedd293160cf43ce18df9d62b8f
yuflavia24 <fla.studio.24@gmail.com>
2025-05-18 18:22:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let filas = [];
 let encabezados = [];
 
 async function cargarDatos() {
-  const respuesta = await fetch('https://sheet.best/api/sheets/abc12345-fghi-6789-jklm-nopqrstuvwx');
+  const respuesta = await fetch('https://api.sheetbest.com/sheets/1a92cbd9-89c3-45bf-8412-362385f0147b');
   const datos = await respuesta.json();
 
+  if (datos.length === 0) return;
+
   encabezados = Object.keys(datos[0]);
-  filas = datos.map(obj => encabezados.map(key => obj[key]));
+  filas = datos.map(obj => encabezados.map(encabezado => obj[encabezado]));
 
   const thead = document.querySelector('thead');
   thead.innerHTML = '<tr>' + encabezados.map(h => `<th>${h}</th>`).join('') + '</tr>';
